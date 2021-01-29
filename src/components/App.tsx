f4c535329fcd3824aa7c39f775db5dcf1bf0ef34
butsuli-shine <luluchey.1223@gmail.com>
2021-01-29 23:38:30 +0900

    add: ルーティングの設定を追加

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home/Home";
+import Project from "./Project/Project"
 
-const App: FC = () => {
+const App: FC = (): JSX.Element => {
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <div className="app">
+      <Router>
+        <Header/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/project" component={Project}/>
+      </Router>
+    </div>
   );
 };
 
