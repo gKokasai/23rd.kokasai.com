3147f52478ec8bafc24720ff1c32dfb1fc098488
butsuli-shine <luluchey.1223@gmail.com>
2021-01-30 23:05:35 +0900

    add: 全てのコンポーネントをAppコンポーネントに集約させるようにした

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,24 @@
 import React, { FC } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Default from "./Default";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Project from "./Project/Project";
+import Access from "./Access/Access";
+import BusInfo from "./BusInfo/BusInfo";
+import Header from "./Header";
 
 const App: FC = (): JSX.Element => {
   return (
     <div className="app">
       <Router>
-        <Route exact path="/" component={Default}/>
+      <div>
+            <Header/>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/project" component={Project}/>
+                <Route path="/access" component={Access}/>
+                <Route path="/busInfo" component={BusInfo}/>
+            </Switch>
+        </div>
       </Router>
     </div>
   );
