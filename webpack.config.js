703f7ef671c5f252ba890ca520e5ae9d4cfa459d
butsuli-shine <luluchey.1223@gmail.com>
2021-01-30 21:42:08 +0900

    add: urlにpathを直接入力してもアクセスできるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
+    publicPath: "/"
   },
 
   resolve: {
@@ -47,7 +48,10 @@ module.exports = {
       }
     ],
   },
-
+  devServer: {
+    historyApiFallback: true,
+  },
+  
   plugins: [
     new HtmlWebpackPlugin({
       template: "./static/index.html",
