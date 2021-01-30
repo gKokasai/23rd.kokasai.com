e243dafa4c77ad129b58f9d4cd90174b2d39c534
butsuli-shine <luluchey.1223@gmail.com>
2021-01-31 01:20:43 +0900

    fix: pathを/forCumpusに変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header: FC = (): JSX.Element => {
         <br/>
         <Link to="/busInfo">バス情報</Link>
         <br/>
-        <Link to="/login">学内向け</Link>
+        <Link to="/forCumpus">学内向け</Link>
     </header>
   );
 };
