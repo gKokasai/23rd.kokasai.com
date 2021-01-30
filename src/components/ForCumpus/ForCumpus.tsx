6c28fbdcb315842f72633d120bdddb860b0aa653
butsuli-shine <luluchey.1223@gmail.com>
2021-01-31 02:32:55 +0900

    add: ログイン処理を実装

--- src/components/ForCumpus/ForCumpus.tsx
+++ src/components/ForCumpus/ForCumpus.tsx
@@ -1,19 +1,52 @@
 import React, { useState, FC } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Base64 } from "js-base64";
 
+const ForCumpus: FC = (): JSX.Element => {
+    const apiUrl: string = "https://api.kokasai.com"
 
-const ForCumpus: FC = () => {
-    const [inputEmail, setInputEmail] = useState({}); 
+    const [inputEmail, setInputEmail] = useState(""); 
     const handleEmailForm = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setInputEmail(event.target.value);
     };
-    const [inputPassWord, setInputPassWord] = useState({});
+
+    const [inputPassWord, setInputPassWord] = useState("");
     const handlePassWordForm = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setInputPassWord(event.target.value);
+    }
+
+    const encodeToBase64: any = (email: any, passWord: any) => {
+        return (Base64.encode(email) + ":" + Base64.encode(passWord));
+    }
+
+    const createHeader: any = () => {
+        const emailAndPassword: string = encodeToBase64(inputEmail, inputPassWord);
+        const headers = {
+            "Authorization": `Basic ${emailAndPassword}`
+        }
+        return headers;
+    }
+
+    const login = () => {
+        const loginEndPointUrl: string = apiUrl + "/login"
+        const header = createHeader();
+        axios
+            .get(loginEndPointUrl, {
+                headers: header
+            })
+            .then((res)=>{
+                console.log(res);
+            })
+        
     }
     return(
         <form onSubmit={() => console.log(inputEmail, inputPassWord)}>
             <input type="text" onChange={() => handleEmailForm}/>
             <input type="text" onChange={() => handlePassWordForm}/>
+            <Link to="/ForCumpus">
+                <button onClick={() => login}>Login</button>
+            </Link>
         </form>
     );
 };
