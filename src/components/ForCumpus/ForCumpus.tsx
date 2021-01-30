d442571c60d39c6a6b41e3c857457df8b1ce3187
butsuli-shine <luluchey.1223@gmail.com>
2021-01-31 01:19:38 +0900

    add: EmailとPasswordのフォームを作成

--- src/components/ForCumpus/ForCumpus.tsx
+++ src/components/ForCumpus/ForCumpus.tsx
@@ -1,9 +1,20 @@
-import React, { FC } from "react";
+import React, { useState, FC } from "react";
 
 
 const ForCumpus: FC = () => {
+    const [inputEmail, setInputEmail] = useState({}); 
+    const handleEmailForm = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setInputEmail(event.target.value);
+    };
+    const [inputPassWord, setInputPassWord] = useState({});
+    const handlePassWordForm = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setInputPassWord(event.target.value);
+    }
     return(
-        
+        <form onSubmit={() => console.log(inputEmail, inputPassWord)}>
+            <input type="text" onChange={() => handleEmailForm}/>
+            <input type="text" onChange={() => handlePassWordForm}/>
+        </form>
     );
 };
 export default ForCumpus;
