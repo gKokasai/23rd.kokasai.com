36845db9f6cba5d776cd38d13401cdcf51af9623
butsuli-shine <luluchey.1223@gmail.com>
2021-01-30 22:41:09 +0900

    add: ルーティング設定を変更

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,12 @@
 import React, { FC } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./Header";
-import Home from "./Home/Home";
-import Project from "./Project/Project"
+import Default from "./Default";
 
 const App: FC = (): JSX.Element => {
   return (
     <div className="app">
       <Router>
-        <Header/>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/project" component={Project}/>
+        <Route exact path="/" component={Default}/>
       </Router>
     </div>
   );
