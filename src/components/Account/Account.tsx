2938d4cdf735767405dd02c350c28d6d16a37c8c
butsuli-shine <luluchey.1223@gmail.com>
2021-02-06 16:14:14 +0900

    add: ログインしている時とログインしていない時の処理を追加

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,13 +1,22 @@
 import React, { FC } from 'react';
+// import { Route } from 'react-router-dom';
 
 type Props = {
     user: string;
     isLoggedIn: boolean;
 }
-const Acccount: FC<Props> = (props) => (
-  <div>
-    {props.user}
-    でログイン中です。
-  </div>
-);
+const Acccount: FC<Props> = (props): JSX.Element => {
+  const { user, isLoggedIn } = props;
+  if (isLoggedIn) {
+    return (
+      <div>
+        {user}
+        でログイン中です。
+      </div>
+    );
+  }
+  return (
+    <div>ログインしていません。</div>
+  );
+};
 export default Acccount;
