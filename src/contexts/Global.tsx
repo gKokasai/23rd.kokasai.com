f3520da94b90ab06996287933b7fac89efb3c6f2
butsuli-shine <cheichey.1223@gmail.com>
2021-04-21 11:36:00 +0900

    fix: undefined で警告が出る問題を修正

--- src/contexts/Global.tsx
+++ src/contexts/Global.tsx
@@ -1,35 +1,71 @@
-import React, { createContext, useState } from 'react';
+import React, { useState } from 'react';
+import * as api from '../api/api';
 
-export const GlobalContext = createContext({});
+type User = {
+    inputId?: string;
+    inputPassWord?: string;
+    isLoggedIn?: boolean;
+    isLoading?: boolean;
+    postedId?: boolean;
+}
 
-export const GlobalProvider = (children) => {
-  const [inputId, setInputId] = useState({ state: '' });
-  const [inputPassWord, setInputPassWord] = useState({ state: '' });
-  /*
-  const [isLoggedIn, setIsLoggedIn] = useState({ state: false });
-  const [isLoading, setIsLoading] = useState({ state: false });
-  const [postedId, setPostedId] = useState({ state: false });
-*/
+type authContextType = {
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+    getToken: () => void;
+    login: () => void;
+    logout: () => void;
+}
+
+function createCtx<ContextType>() {
+  const ctx = React.createContext<ContextType | undefined>(undefined);
+  const useCtx = () => {
+    const c = React.useContext(ctx);
+    if (!c) throw new Error('useCtx must be inside a Provider with a value');
+    return c;
+  };
+  return [useCtx, ctx.Provider] as const;
+}
+
+const [useAuth, SetAuthProvider] = createCtx<authContextType>();
+
+const useAuthCtx = (): authContextType => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const getToken = () => {
+    setUser({ postedId: true });
+    const result = api.getToken(user?.inputId);
+    console.log(result, setUser);
+  };
+
+  const login = () => {
+    setUser({ isLoading: true });
+    const result = api.login(user?.inputId, user?.inputPassWord)
+      .then(() => { setUser({ isLoading: false, isLoggedIn: true }); });
+    console.log(result);
+  };
+
+  const logout = () => {
+    setUser({ isLoading: true });
+    const result = api.logout()
+      .then(() => { setUser({ isLoading: false, isLoggedIn: false }); });
+    console.log(result);
+  };
+
+  return {
+    user, getToken, login, logout, setUser,
+  };
+};
+
+const AuthProvider = ({ children }) => {
+  const auth = useAuthCtx();
   return (
-    <GlobalContext.Provider
-      value={
-          {
-            inputId,
-            setInputId,
-            inputPassWord,
-            setInputPassWord,
-            /*
-            isLoggedIn,
-            setIsLoggedIn,
-            isLoading,
-            setIsLoading,
-            postedId,
-            setPostedId,
-               */
-          }
-      }
+    <SetAuthProvider
+      value={auth}
     >
       {children}
-    </GlobalContext.Provider>
+    </SetAuthProvider>
   );
 };
+
+export { useAuth, AuthProvider };
