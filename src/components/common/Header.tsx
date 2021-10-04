dfbbe98977cdba8e8f69dcbe33d4cdfa99d3933d
butsuli-shine <luluchey.1223@gmail.com>
2021-10-05 07:25:25 +0900

    feat: ヘッダーを追加した

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import Static from "../../static";
 
-const Header: React.FC = () => <>header</>;
+const Header: React.FC = () => (
+  <div>
+    <header className="bg-blue-300">
+      <h1>工華祭</h1>
+      <a href="https://twitter.com/NITGC_Kokasai">
+        <img
+          className="container h-6 w-6 flex"
+          src={Static.twitterIcon}
+          alt="Twitterのsvgアイコン"
+        />
+      </a>
+      <a href="https://Instagram.com/nitgc_kokasai">
+        <img
+          className="container h-7 w-7 flex"
+          src={Static.instagramIcon}
+          alt="Instagramのsvgアイコン"
+        />
+      </a>
+    </header>
+  </div>
+);
 
 export default Header;
