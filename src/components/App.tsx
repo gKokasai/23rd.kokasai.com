42b9af504f1c365a3b0dd80eb11e012eb985a9d4
butsuli-shine <luluchey.1223@gmail.com>
2021-10-05 07:25:25 +0900

    feat: ヘッダーを追加した

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Top from "./top/Top";
+import Header from "./common/Header";
 
-const App: React.FC = () => <Top />;
+const App: React.FC = () => (
+  <div>
+    <Header />
+    <Top />
+  </div>
+);
 
 export default App;
