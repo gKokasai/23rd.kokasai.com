3dc62bca2298ae304d4898f9b483490433a3e262
cheichey <cheichey.1223@gmail.com>
2021-10-07 17:43:01 +0900

    fix-ui: イラストの大きさを調整

--- src/components/top/right/LogoIllust.tsx
+++ src/components/top/right/LogoIllust.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Static from "../../../static";
 
 const LogoIllust: React.FC = () => (
-  <div className="w-11/12 mx-auto">
+  <div className="w-10/12 mx-auto">
     <img src={Static.kokasaiIllust.icon} alt="工華祭のロゴ" />
   </div>
 );
