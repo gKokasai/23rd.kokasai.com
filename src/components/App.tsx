9dd4e40068622a1bf35e33efa25148aefd5683ce
cheichey <cheichey.1223@gmail.com>
2021-10-11 23:30:44 +0900

    fix-ui: 右側を固定し、左側をスクロールできるようにした。 ヘッダーを右側の下部に設置した。

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,22 +7,28 @@ import Project from "./project/Project";
 import Left from "./common/Left";
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <div className="container h-full w-full font-serif bg-cream">
-      <div className="flex flex-row h-full w-full">
-        <div className="hidden pc:inline-block pc:h-full pc:w-1/4 ">
-          <Left />
-        </div>
-        <div className="h-full w-3/4">
-          <Switch>
-            <Route exact path={Pages.top.path} component={Top} />
-            <Route exact path={Pages.project.path} component={Project} />
-          </Switch>
+  <div className="bg-cream h-full w-full relative">
+    <BrowserRouter>
+      <div className="container h-full w-full font-serif bg-cream">
+        <div className="h-full w-full">
+          <div className="flex h-full pc:w-full">
+            <div className="hidden pc:inline-block pc:h-screen pc:w-1/4">
+              <Left />
+            </div>
+            <div className="h-full pc:w-3/4 pc:overflow-y-scroll">
+              <Switch>
+                <Route exact path={Pages.top.path} component={Top} />
+                <Route exact path={Pages.project.path} component={Project} />
+              </Switch>
+              <div className="w-full">
+                <Footer />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <Footer />
-    </div>
-  </BrowserRouter>
+    </BrowserRouter>
+  </div>
 );
 
 export default App;
--- src/components/common/Left.tsx
+++ src/components/common/Left.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Pages from "../Pages";
 
 const Left: React.FC = () => (
@@ -6,9 +7,9 @@ const Left: React.FC = () => (
     <div className="bg-midBlue flex h-full flex-col items-end justify-end pr-6 text-white">
       <ul>
         {Object.keys(Pages).map((key) => (
-          <a href="/" key={key}>
+          <Link to={Pages[key].path} key={key}>
             <li className="pb-8 text-lg">{Pages[key].displayName}</li>
-          </a>
+          </Link>
         ))}
       </ul>
     </div>
