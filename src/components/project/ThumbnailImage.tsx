b74564d727d4133d132013380d432cac882bc958
butsuli-shine <luluchey.1223@gmail.com>
2021-10-11 21:12:17 +0900

    feat: 画像にカーソルを合わせたら拡大するトランジションを追加した

--- src/components/project/ThumbnailImage.tsx
+++ src/components/project/ThumbnailImage.tsx
@@ -16,7 +16,11 @@ const ThumbnailImage: React.FC<ThumbnailImageProps> = (props) => {
   const { path, alt } = props;
   return (
     <div className="h-30 w-30">
-      <img src={path} alt={alt} />
+      <img
+        src={path}
+        alt={alt}
+        className="transition duration-500 ease-in-out transform hover:scale-110"
+      />
     </div>
   );
 };
