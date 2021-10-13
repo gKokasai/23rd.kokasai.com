61b02be0ef865c6995e301d3b24ba4beab69899c
cheichey <cheichey.1223@gmail.com>
2021-10-13 23:01:45 +0900

    feat: Cardにヘッダーを追加した. WithHeaderCard にファイル名を変更した

--- src/components/project/WithHeaderCard.tsx
+++ src/components/project/WithHeaderCard.tsx
@@ -4,15 +4,16 @@ export type BlockProps = {
   header: string;
 };
 
-const Block: React.FC<BlockProps> = (props) => {
-  const { header } = props;
+const WithHeaderCard: React.FC<BlockProps> = (props) => {
+  const { header, children } = props;
   return (
     <div className="pc:w-60 pc:h-60 desktop:w-96 desktop:h-96 bg-greenC7D1BD rounded-2xl shadow-2xl transition duration-500 ease-in-out transform hover:scale-110">
-      <h3 className="pc:pt-8 text-center desktop:pt-12 desktop:pl-6 pc:text-lg desktop:text-2xl">
+      <h3 className="pc:pt-4 text-center desktop:pt-12 desktop:pl-6 desktop:text-2xl">
         {header}
       </h3>
+      {children}
     </div>
   );
 };
 
-export default Block;
+export default WithHeaderCard;
