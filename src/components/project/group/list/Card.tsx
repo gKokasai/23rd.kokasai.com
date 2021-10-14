bf0fefaf3e39ded22826f399e6ff3582129dc644
cheichey <cheichey.1223@gmail.com>
2021-10-15 06:35:58 +0900

    fix-ui: Cardの子要素のスタイルを修正した

--- src/components/project/group/list/Card.tsx
+++ src/components/project/group/list/Card.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from "react";
 import ThumbnailImage from "./ThumbnailImage";
-import WithHeaderCard from "../../WithHeaderCard";
 
 /**
  * 企画一覧のカードコンポーネント
@@ -19,19 +18,20 @@ export type CardProps = {
 const Card: FC<CardProps> = (props) => {
   const { thumbnailImagePath, name, groupName } = props;
   return (
-    <WithHeaderCard header={name}>
-      <div className="container">
-        <div className="flex flex-row justify-center">
-          <ThumbnailImage
-            path={thumbnailImagePath}
-            alt={`${name}のサムネイル画像`}
-          />
-        </div>
-        <div className="text-center">
-          <div className="pb-4">{groupName}</div>
+    <div className="h-full w-full bg-greenC7D1BD rounded-2xl shadow-2xl transition duration-500 ease-in-out transform hover:scale-110">
+      <div className="container h-full">
+        <div className="flex flex-col items-center">
+          <h3 className="desktop:text-2xl pt-4">{name}</h3>
+          <div className="pt-4">
+            <ThumbnailImage
+              path={thumbnailImagePath}
+              alt={`${name}のサムネイル画像`}
+            />
+          </div>
+          <div className="pt-4 pb-4">{groupName}</div>
         </div>
       </div>
-    </WithHeaderCard>
+    </div>
   );
 };
 
