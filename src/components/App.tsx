47cba45557c7bf110ab7e8a6025fe1981e455174
cheichey <cheichey.1223@gmail.com>
2021-10-15 08:22:30 +0900

    fix: Suspense の位置を変えた

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,42 +13,36 @@ const ProjectGroup = lazy(() => import("./project/group/ProjectGroup"));
 const App: React.FC = () => (
   <div className="bg-cream h-full w-full relative">
     <BrowserRouter>
-      <Switch>
-        <div className="container h-full w-full font-serif bg-cream">
-          <div className="h-full w-full">
-            <div className="flex h-full pc:w-full">
-              <div className="hidden pc:inline-block pc:h-screen pc:w-1/4">
-                <Left />
-              </div>
-              <div className="h-full pc:w-3/4 pc:overflow-y-scroll">
-                <Suspense fallback={<Loading />}>
+      <div className="container h-full w-full font-serif bg-cream">
+        <div className="h-full w-full">
+          <div className="flex h-full pc:w-full">
+            <div className="hidden pc:inline-block pc:h-screen pc:w-1/4">
+              <Left />
+            </div>
+            <div className="h-full pc:w-3/4 pc:overflow-y-scroll">
+              <Suspense fallback={Loading}>
+                <Switch>
                   <Route exact path={Pages.top.path} component={Top} />
-                </Suspense>
-                <Suspense fallback={<Loading />}>
                   <Route exact path={Pages.project.path} component={Project} />
-                </Suspense>
-                <Suspense fallback={<Loading />}>
                   <Route
                     exact
                     path={Pages.projectGroup.path}
                     component={ProjectGroup}
                   />
-                </Suspense>
-                <Suspense fallback={<Loading />}>
                   <Route
                     exact
                     path={Pages.projectMap.path}
                     component={ProjectMap}
                   />
-                </Suspense>
-                <div className="w-full">
-                  <Footer />
-                </div>
+                </Switch>
+              </Suspense>
+              <div className="w-full">
+                <Footer />
               </div>
             </div>
           </div>
         </div>
-      </Switch>
+      </div>
     </BrowserRouter>
   </div>
 );
