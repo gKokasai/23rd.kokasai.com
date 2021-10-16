04b4173c55b4db7c1545769b063b05637265a77b
Keisuke Sakamoto <66987550+cheichey@users.noreply.github.com>
2021-10-16 20:54:58 +0900

    Update src/components/project/WithHeaderHoverCard.tsx
    Co-authored-by: sya-ri <34268371+sya-ri@users.noreply.github.com>

--- src/components/project/WithHeaderHoverCard.tsx
+++ src/components/project/WithHeaderHoverCard.tsx
@@ -8,7 +8,7 @@ const WithHeaderHoverCard: React.FC<BlockProps> = (props) => {
   const { header } = props;
   return (
     <div className="pc:w-60 pc:h-60 desktop:w-96 desktop:h-96 bg-greenC7D1BD rounded-2xl shadow-2xl transition duration-500 ease-in-out transform hover:scale-110 flex justify-center items-center">
-      <div className="text-2xl">{header}</div>
+      <div className="text-2xl text-center px-2">{header}</div>
     </div>
   );
 };
