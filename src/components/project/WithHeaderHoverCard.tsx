af84c5b2a7708cbaf8bba92e033b5674fe414272
cheichey <cheichey.1223@gmail.com>
2021-10-16 15:46:09 +0900

    fix-ui: "地図から探す" と "クラス名・団体名から探す" を中央揃えにした

--- src/components/project/WithHeaderHoverCard.tsx
+++ src/components/project/WithHeaderHoverCard.tsx
@@ -5,13 +5,10 @@ export type BlockProps = {
 };
 
 const WithHeaderHoverCard: React.FC<BlockProps> = (props) => {
-  const { header, children } = props;
+  const { header } = props;
   return (
-    <div className="pc:w-60 pc:h-60 desktop:w-96 desktop:h-96 bg-greenC7D1BD rounded-2xl shadow-2xl transition duration-500 ease-in-out transform hover:scale-110">
-      <h3 className="pc:pt-4 text-center desktop:pt-6 desktop:pl-6 desktop:text-2xl">
-        {header}
-      </h3>
-      {children}
+    <div className="pc:w-60 pc:h-60 desktop:w-96 desktop:h-96 bg-greenC7D1BD rounded-2xl shadow-2xl transition duration-500 ease-in-out transform hover:scale-110 flex justify-center items-center">
+      <div className="text-2xl">{header}</div>
     </div>
   );
 };
