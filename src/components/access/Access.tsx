8d48c1ee18f1b06b67bdfa70be349796ed4d9dac
cheichey <cheichey.1223@gmail.com>
2021-10-17 13:31:01 +0900

    refactor: Google Mapの urlを static から読み込むようにした

--- src/components/access/Access.tsx
+++ src/components/access/Access.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import PageTitle from "../common/PageTitle";
 import SubTitle from "../common/SubTitle";
 import Paragraph from "../common/Paragraph";
+import Static from "../../static";
 
 const Access: React.FC = () => (
   <div>
@@ -10,7 +11,7 @@ const Access: React.FC = () => (
     <Paragraph>
       <iframe
         title="google map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2708.458921419906!2d139.02010350336255!3d36.37627653385497!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601e8cc32e0daf8d%3A0x207d7e6356e87ed8!2z576k6aas5bel5qWt6auY562J5bCC6ZaA5a2m5qCh!5e0!3m2!1sja!2sjp!4v1634367380548!5m2!1sja!2sjp"
+        src={Static.googleMap.src}
         width="600"
         height="450"
         loading="lazy"
