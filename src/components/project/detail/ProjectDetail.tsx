ae3f9a2aa6fa218ceecea7bda8891979c11dff87
cheichey <cheichey.1223@gmail.com>
2021-10-19 10:58:50 +0900

    fix-ui: ProjectDetail のレイアウトと背景色を変更した

--- src/components/project/detail/ProjectDetail.tsx
+++ src/components/project/detail/ProjectDetail.tsx
@@ -13,32 +13,32 @@ const ProjectDetail: React.FC = () => {
     window.scroll({ top: 0, behavior: "auto" });
   }, []);
   return (
-    <div className="h-full w-full relative m-auto box-border bg-pinkF19393 bg-clip-content bg-cover p-6 pc:p-16">
-      <div className="w-full flex justify-between">
-        <div>
-          <div className="flex w-full">
-            <div className="w-1/2 pt-10 pl-6 pc:pt-10 pc:pr-10 pc:hidden">
-              <ThumbnailImage
-                path={Static.projectThumbnail.path(groupName)}
-                alt={Static.projectThumbnail.alt(groupName)}
-              />
-            </div>
-            <h2 className="w-full text-lg pl-10 pt-10">{name}</h2>
-          </div>
-          <div className="pl-36 pc:pl-20 pc:pt-6 flex">
-            <div className="pr-2 pb-4">
-              <GroupOutlinedIcon />
-            </div>
-            {groupName}
-          </div>
-          <div className="pt-10 px-5">{description}</div>
-        </div>
-        <div className="hidden pc:pt-10 pc:pr-10 pc:inline-block">
+    <div className="h-full w-full relative bg-clip-content bg-cover p-10">
+      <div className="bg-blue-200">
+        <div className="p-4 w-1/3 pc:hidden">
           <ThumbnailImage
             path={Static.projectThumbnail.path(groupName)}
             alt={Static.projectThumbnail.alt(groupName)}
           />
         </div>
+        <div className="flex justify-between">
+          <div>
+            <div className="pc:text-2xl p-2 px-4 pc:p-4">{name}</div>
+            <div className="pc:text-lg flex p-4">
+              <GroupOutlinedIcon />
+              <div className="pl-2">{groupName}</div>
+            </div>
+          </div>
+          <div>
+            <div className="p-4 w-1/3 hidden pc:inline-block">
+              <ThumbnailImage
+                path={Static.projectThumbnail.path(groupName)}
+                alt={Static.projectThumbnail.alt(groupName)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="p-4">{description}</div>
       </div>
     </div>
   );
