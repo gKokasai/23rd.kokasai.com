c9b6cac04cd923b9b8a562c4160b6b13946b6c67
cheichey <cheichey.1223@gmail.com>
2021-10-20 00:23:38 +0900

    feat: 企画一覧で直接一覧を表示するようにした

--- src/components/common/Paragraph.tsx
+++ src/components/common/Paragraph.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Paragraph: React.FC = ({ children }) => (
-  <p className="pc:text-lg desktop:text-xl px-4 pt-3 pc:pl-20 pc:pt-5 pc:pr-8">{children}</p>
+  <p className="pc:text-lg desktop:text-xl px-4 pt-3 pc:pl-20 pc:pt-5 pc:pr-8">
+    {children}
+  </p>
 );
 
 export default Paragraph;
