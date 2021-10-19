38e8070aa37ebd2b1106d18496fe52eaec86ac61
cheichey <cheichey.1223@gmail.com>
2021-10-19 22:58:37 +0900

    fix: 使用しないことになった地図から探すを削除した、によって影響したスタイルを修正

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import WithHeaderHoverCard from "./WithHeaderHoverCard";
+import PageTitle from "../common/PageTitle";
 
 const Project: React.FC = () => (
-  <div className="w-full h-full">
-    <h2 className="text-lg pl-5 pt-5 pc:text-2xl desktop:text-4xl pc:pl-10 pc:pt-10">
-      企画一覧
-    </h2>
-    <div className="flex flex-col pt-16 items-center pc:flex-row pc:justify-around pc:pt-32 desktop:pt-44">
-      <div className="pb-10">
-        <Link to="/project/map">
-          <WithHeaderHoverCard header="地図から探す" />
-        </Link>
-      </div>
-      <div className="">
+  <div className="w-full h-full container">
+    <PageTitle>企画一覧</PageTitle>
+    <div className="h-3/4 flex flex-col items-center justify-around m-auto">
+      <div>
         <Link to="/project/group">
           <WithHeaderHoverCard header="クラス名・団体名から探す" />
         </Link>
--- src/components/project/map/ProjectMap.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const ProjectMap: React.FC = () => <div>Map</div>;
-
-export default ProjectMap;
