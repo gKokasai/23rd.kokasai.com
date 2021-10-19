e99e3e0e266b0abb65bfa9d4c2123f99e5889fc3
cheichey <cheichey.1223@gmail.com>
2021-10-19 16:30:58 +0900

    fix-ui: ごあいさつページのスタイルを修正

--- src/components/common/Paragraph.tsx
+++ src/components/common/Paragraph.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const Paragraph: React.FC = ({ children }) => (
   <p className="pc:text-lg desktop:text-xl px-4 pt-3 pc:pl-20 pc:pt-5 pc:pr-8">{children}</p>
-</p>
 );
 
 export default Paragraph;
