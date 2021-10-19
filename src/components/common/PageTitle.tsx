3ba3be414a186d9598fe460d733cb8777403d7a0
cheichey <cheichey.1223@gmail.com>
2021-10-19 12:52:39 +0900

    fix: アクセスページのスタイルを修正した、地図をレスポンシブにした

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageTitle: React.FC = ({ children }) => (
-  <h2 className="pc:text-2xl desktop:text-4xl pl-10 pt-10">{children}</h2>
+  <h2 className="text-xl pc:text-2xl desktop:text-4xl pl-5 pc:pl-10 pt-10">{children}</h2>
 );
 
 export default PageTitle;
