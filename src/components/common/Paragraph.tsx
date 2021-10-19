c8421ec5e8888c6662fcec483113b604c9e4715b
Keisuke Sakamoto <66987550+cheichey@users.noreply.github.com>
2021-10-19 14:54:19 +0900

    Update src/components/common/Paragraph.tsx
    Co-authored-by: sya-ri <34268371+sya-ri@users.noreply.github.com>

--- src/components/common/Paragraph.tsx
+++ src/components/common/Paragraph.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Paragraph: React.FC = ({ children }) => (
-  <p className="pc:text-lg desktop:text-xl pl-10 pt-3 pr-4 pc:pl-20 pc:pt-5 pc:pr-8">{children}</p>
+  <p className="pc:text-lg desktop:text-xl px-4 pt-3 pc:pl-20 pc:pt-5 pc:pr-8">{children}</p>
+</p>
 );
 
 export default Paragraph;
