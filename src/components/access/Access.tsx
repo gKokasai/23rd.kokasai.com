1abbe26f58a48ad36c2b4ab6382080da9f5541e1
cheichey <cheichey.1223@gmail.com>
2021-10-19 12:52:39 +0900

    fix: アクセスページのスタイルを修正した、地図をレスポンシブにした

--- src/components/access/Access.tsx
+++ src/components/access/Access.tsx
@@ -5,30 +5,52 @@ import Paragraph from "../common/Paragraph";
 import Static from "../../static";
 
 const Access: React.FC = () => (
-  <div>
+  <div className="pb-4 bg-cream">
     <PageTitle>アクセス</PageTitle>
     <SubTitle>周辺地図</SubTitle>
     <Paragraph>
-      <iframe
-        title="google map"
-        src={Static.googleMap.src}
-        width="600"
-        height="450"
-        loading="lazy"
-      />
+      <div className="pt-96 pb-36 pr-4 h-0 relative">
+        <iframe
+          title="google map"
+          src={Static.googleMap.src}
+          className="absolute w-full h-full left-0 right-0 bottom-0 top-0"
+          width="600"
+          height="450"
+          loading="lazy"
+        />
+      </div>
     </Paragraph>
     <SubTitle>JR新前橋駅より</SubTitle>
-    <Paragraph>徒歩約30分またはタクシー約10分</Paragraph>
+    <Paragraph>
+      <li>徒歩約30分</li>
+      <li>タクシー約10分</li>
+    </Paragraph>
 
     <SubTitle>JR井野駅より</SubTitle>
     <Paragraph>
-      井野駅北バス停より、市内循環バス「ぐるりん」大八木線・中尾先回り（系統番号6）で群馬高専前下車（約20分）
+      <li>バス約20分
+        <div className="pl-6 pt-2">
+          <p>井野駅北バス停</p>
+          <p className="pl-10">↓</p>
+          <p>市内循環バス「ぐるりん」大八木線・中尾先回りで約20分 (系統番号6)</p>
+          <p className="pl-10">↓</p>
+          <p>群馬高専前下車</p>
+        </div>
+      </li>
     </Paragraph>
 
-    <SubTitle>JR高崎駅より（約8km）</SubTitle>
+    <SubTitle>JR高崎駅より</SubTitle>
     <Paragraph>
-      高崎駅西口バス停9番のりばより、市内循環バス「ぐるりん」大八木線・中尾先回り（系統番号6）で群馬高専前下車（約44分）
-      タクシー約30分
+      <li>バス約44分
+        <div className="pl-6 pt-2">
+          <p>高崎駅西口バス停9番のりば</p>
+          <p className="pl-10">↓</p>
+          <p>市内循環バス「ぐるりん」大八木線・中尾先回り（系統番号6)</p>
+          <p className="pl-10">↓</p>
+          <p>群馬高専前下車</p>
+        </div>
+      </li>
+      <li className="pt-2">タクシー約30分</li>
     </Paragraph>
 
     <SubTitle>関越自動車道 前橋インターチェンジより（約1km）</SubTitle>
--- src/components/common/SubTitle.tsx
+++ src/components/common/SubTitle.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const SubTitle: React.FC = ({ children }) => (
-  <h3 className="pc:text-xl desktop:text-2xl pl-16 pt-5 border-b border-black w-5/6">
-    {children}
-  </h3>
+  <div className="pl-1">
+    <h3 className="pc:text-xl desktop:text-2xl pl-8 pc:pl-16 pt-5 border-b border-black w-5/6">
+      {children}
+    </h3>
+  </div>
 );
 
 export default SubTitle;
