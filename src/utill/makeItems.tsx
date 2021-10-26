9301ad3a760d75c716d58d0034393f6e3197fff3
cheichey <cheichey.1223@gmail.com>
2021-10-27 08:14:28 +0900

    fix: 生配信のURLに飛ばすUIの仕様を変更した 画面幅によってタイムラインのデザインを変えるようにした

--- src/utill/makeItems.tsx
+++ src/utill/makeItems.tsx
@@ -1,65 +1,32 @@
-import React from "react";
 import { TimelineItemModel } from "react-chrono/dist/models/TimelineItemModel";
 import { Timetable } from "../repository/Timetabel";
 import Static from "../static";
 
 export type KokasaiTimeLineItemModel = {
-  day1: {
-    items: TimelineItemModel[];
-    icons: JSX.Element;
-  };
-  day2: {
-    items: TimelineItemModel[];
-    icons: JSX.Element;
-  };
+  day1: (TimelineItemModel & { url: string })[];
+  day2: (TimelineItemModel & { url: string })[];
 };
 export const makeKokasaiTimeLine = (
   timeTable: Timetable
 ): KokasaiTimeLineItemModel => ({
-  day1: {
-    items: timeTable.day1.map((time) => ({
-      title: time.time,
-      cardTitle: time.title,
-      media: {
-        type: "IMAGE",
-        source: { url: Static.stageImage.src(time.groupName) },
-      },
-    })),
-    icons: (
-      <div className="chrono-icons">
-        {timeTable.day1.map((time, index) =>
-          time.url ? (
-            <a href={Static.stageLink.src("day1", time.url)}>
-              <img src={Static.youtube.icon} alt={`${index + 1}`} />
-            </a>
-          ) : null
-        )}
-      </div>
-    ),
-  },
-  day2: {
-    items: timeTable.day2.map((time) => ({
-      title: time.time,
-      cardTitle: time.title,
-      media: {
-        type: "IMAGE",
-        source: { url: Static.stageImage.src(time.groupName) },
-      },
-    })),
-    icons: (
-      <div className="chrono-icons">
-        {timeTable.day2.map((time, index) =>
-          time.url !== "" ? (
-            <a href={Static.stageLink.src("day2", time.url)}>
-              <img
-                width="auto"
-                src={Static.youtube.icon}
-                alt={`${index + 1}`}
-              />
-            </a>
-          ) : null
-        )}
-      </div>
-    ),
-  },
+  day1: timeTable.day1.map((time) => ({
+    url: time.url ? Static.stageLink.src("day1", time.url) : "",
+    title: time.time,
+    cardTitle: time.title,
+    cardDetailedText: time.url ? "生配信はこちら" : null,
+    media: {
+      type: "IMAGE",
+      source: { url: Static.stageImage.src(time.groupName) },
+    },
+  })),
+  day2: timeTable.day2.map((time) => ({
+    title: time.time,
+    cardTitle: time.title,
+    cardDetailedText: time.url ? "生配信はこちら" : null,
+    media: {
+      type: "IMAGE",
+      source: { url: Static.stageImage.src(time.groupName) },
+    },
+    url: Static.stageLink.src("day2", time.url),
+  })),
 });
